c0158cf977c137240ffccd2a4b899520f190b6a2
WildSwap <85905532+WildSwap@users.noreply.github.com>
2021-06-18 16:30:26 +0200

    Update contenthashToUri.ts

--- src/utils/contenthashToUri.ts
+++ src/utils/contenthashToUri.ts
@@ -3,7 +3,7 @@ import { getCodec, rmPrefix } from 'multicodec'
 import { decode, toB58String } from 'multihashes'
 
 export function hexToUint8Array(hex: string): Uint8Array {
-  hex = hex.startsWith('0x') ? hex.substr(2) : hex
+  hex = hex.startsWith('0x')  hex.substr(2) : hex
   if (hex.length % 2 !== 0) throw new Error('hex must have length that is multiple of 2')
   const arr = new Uint8Array(hex.length / 2)
   for (let i = 0; i < arr.length; i++) {
