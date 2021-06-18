df0a2ea05aaad0a56d6b0e0a402b7f16b32eac1d
WildSwap <85905532+WildSwap@users.noreply.github.com>
2021-06-18 16:35:27 +0200

    Update maxAmountSpend.ts

--- src/utils/maxAmountSpend.ts
+++ src/utils/maxAmountSpend.ts
@@ -5,7 +5,7 @@ import { MIN_ETH } from '../constants'
  * Given some token amount, return the max that can be spent of it
  * @param currencyAmount to return max of
  */
-export function maxAmountSpend(currencyAmount?: CurrencyAmount): CurrencyAmount | undefined {
+export function maxAmountSpend(currencyAmount: CurrencyAmount): CurrencyAmount | undefined {
   if (!currencyAmount) return undefined
   if (currencyAmount.currency === ETHER) {
     if (JSBI.greaterThan(currencyAmount.raw, MIN_ETH)) {
