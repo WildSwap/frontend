9422310e48fc3d9d3c72e9e0bb2cc264441b132b
WildSwap <85905532+WildSwap@users.noreply.github.com>
2021-06-18 12:20:05 +0200

    Update index.tsx

--- src/components/Row/index.tsx
+++ src/components/Row/index.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 import { Box } from 'rebass/styled-components'
 
-const Row = styled(Box)<{ align?: string; padding?: string; border?: string; borderRadius?: string }>`
+const Row = styled(Box)<{ align: string; padding: string; border: string; borderRadius: string }>`
   width: 100%;
   display: flex;
   padding: 0;
@@ -20,7 +20,7 @@ export const RowFlat = styled.div`
   align-items: flex-end;
 `
 
-export const AutoRow = styled(Row)<{ gap?: string; justify?: string }>`
+export const AutoRow = styled(Row)<{ gap: string; justify: string }>`
   flex-wrap: wrap;
   margin: ${({ gap }) => gap && `-${gap}`};
   justify-content: ${({ justify }) => justify && justify};
@@ -30,7 +30,7 @@ export const AutoRow = styled(Row)<{ gap?: string; justify?: string }>`
   }
 `
 
-export const RowFixed = styled(Row)<{ gap?: string; justify?: string }>`
+export const RowFixed = styled(Row)<{ gap: string; justify: string }>`
   width: fit-content;
   margin: ${({ gap }) => gap && `-${gap}`};
 `
