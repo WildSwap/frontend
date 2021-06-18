ed8409073bb79914351bbf468adc99ca7ce2c5aa
WildSwap <85905532+WildSwap@users.noreply.github.com>
2021-06-18 14:24:56 +0200

    Update PoolPriceBar.tsx

--- src/pages/AddLiquidity/PoolPriceBar.tsx
+++ src/pages/AddLiquidity/PoolPriceBar.tsx
@@ -15,31 +15,31 @@ export function PoolPriceBar({
   poolTokenPercentage,
   price,
 }: {
-  currencies: { [field in Field]?: Currency }
-  noLiquidity?: boolean
-  poolTokenPercentage?: Percent
-  price?: Price
+  currencies: { [field in Field]: Currency }
+  noLiquidity: boolean
+  poolTokenPercentage: Percent
+  price: Price
 }) {
   return (
     <AutoColumn gap="md">
       <AutoRow justify="space-around" gap="4px">
         <AutoColumn justify="center">
-          <Black>{price?.toSignificant(6) ?? '-'}</Black>
+          <Black>{price.toSignificant(6)  '-'}</Black>
           <Text fontSize="14px" color="textSubtle" pt={1}>
-            {currencies[Field.CURRENCY_B]?.symbol} per {currencies[Field.CURRENCY_A]?.symbol}
+            {currencies[Field.CURRENCY_B].symbol} per {currencies[Field.CURRENCY_A].symbol}
           </Text>
         </AutoColumn>
         <AutoColumn justify="center">
-          <Black>{price?.invert()?.toSignificant(6) ?? '-'}</Black>
+          <Black>{price.invert().toSignificant(6)  '-'}</Black>
           <Text fontSize="14px" color="textSubtle" pt={1}>
-            {currencies[Field.CURRENCY_A]?.symbol} per {currencies[Field.CURRENCY_B]?.symbol}
+            {currencies[Field.CURRENCY_A].symbol} per {currencies[Field.CURRENCY_B].symbol}
           </Text>
         </AutoColumn>
         <AutoColumn justify="center">
           <Black>
             {noLiquidity && price
-              ? '100'
-              : (poolTokenPercentage?.lessThan(ONE_BIPS) ? '<0.01' : poolTokenPercentage?.toFixed(2)) ?? '0'}
+               '100'
+              : (poolTokenPercentage?.lessThan(ONE_BIPS)  '<0.01' : poolTokenPercentage?.toFixed(2))  '0'}
             %
           </Black>
           <Text fontSize="14px" color="textSubtle" pt={1}>
