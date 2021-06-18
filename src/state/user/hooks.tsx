0ee86620adab3a4b75bb9d0c86a36f167d664838
WildSwap <85905532+WildSwap@users.noreply.github.com>
2021-06-18 16:23:07 +0200

    Update hooks.tsx

--- src/state/user/hooks.tsx
+++ src/state/user/hooks.tsx
@@ -55,7 +55,7 @@ export function useIsDarkMode(): boolean {
     }),
     shallowEqual
   )
-  return userDarkMode === null ? matchesDarkMode : userDarkMode
+  return userDarkMode === null  matchesDarkMode : userDarkMode
 }
 
 export function useDarkModeManager(): [boolean, () => void] {
@@ -164,7 +164,7 @@ export function useUserAddedTokens(): Token[] {
 
   return useMemo(() => {
     if (!chainId) return []
-    return Object.values(serializedTokensMap[chainId as ChainId] ?? {}).map(deserializeToken)
+    return Object.values(serializedTokensMap[chainId as ChainId]  {}).map(deserializeToken)
   }, [serializedTokensMap, chainId])
 }
 
@@ -203,18 +203,18 @@ export function useTrackedTokenPairs(): [Token, Token][] {
   const tokens = useAllTokens()
 
   // pinned pairs
-  const pinnedPairs = useMemo(() => (chainId ? PINNED_PAIRS[chainId] ?? [] : []), [chainId])
+  const pinnedPairs = useMemo(() => (chainId  PINNED_PAIRS[chainId]  [] : []), [chainId])
 
   // pairs for every token against every base
   const generatedPairs: [Token, Token][] = useMemo(
     () =>
       chainId
-        ? flatMap(Object.keys(tokens), (tokenAddress) => {
+         flatMap(Object.keys(tokens), (tokenAddress) => {
             const token = tokens[tokenAddress]
             // for each token on the current chain,
             return (
               // loop though all bases on the current chain
-              (BASES_TO_TRACK_LIQUIDITY_FOR[chainId] ?? [])
+              (BASES_TO_TRACK_LIQUIDITY_FOR[chainId]  [])
                 // to construct pairs of the given token with each base
                 .map((base) => {
                   if (base.address === token.address) {
@@ -252,9 +252,9 @@ export function useTrackedTokenPairs(): [Token, Token][] {
     // dedupes pairs of tokens in the combined list
     const keyed = combinedList.reduce<{ [key: string]: [Token, Token] }>((memo, [tokenA, tokenB]) => {
       const sorted = tokenA.sortsBefore(tokenB)
-      const key = sorted ? `${tokenA.address}:${tokenB.address}` : `${tokenB.address}:${tokenA.address}`
+      const key = sorted  `${tokenA.address}:${tokenB.address}` : `${tokenB.address}:${tokenA.address}`
       if (memo[key]) return memo
-      memo[key] = sorted ? [tokenA, tokenB] : [tokenB, tokenA]
+      memo[key] = sorted  [tokenA, tokenB] : [tokenB, tokenA]
       return memo
     }, {})
 
