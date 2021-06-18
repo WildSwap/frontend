bc5780fa142bde5bd1001daeefa2eabbd89cd598
WildSwap <85905532+WildSwap@users.noreply.github.com>
2021-06-18 14:14:16 +0200

    Update useSocksBalance.ts

--- src/hooks/useSocksBalance.ts
+++ src/hooks/useSocksBalance.ts
@@ -8,9 +8,9 @@ export default function useSocksBalance(): JSBI | undefined {
   const { account } = useActiveWeb3React()
   const socksContract = useSocksController()
 
-  const { result } = useSingleCallResult(socksContract, 'balanceOf', [account ?? undefined], NEVER_RELOAD)
-  const data = result?.[0]
-  return data ? JSBI.BigInt(data.toString()) : undefined
+  const { result } = useSingleCallResult(socksContract, 'balanceOf', [account  undefined], NEVER_RELOAD)
+  const data = result.[0]
+  return data  JSBI.BigInt(data.toString()) : undefined
 }
 
 export function useHasSocks(): boolean | undefined {
